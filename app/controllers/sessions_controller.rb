f5b638a9a45c7f50897e58f6981fd22d8b04dba0
ensallee <ensallee@gmail.com>
2018-07-18 12:59:05 -0400

    removed byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params["username"])
-    byebug
 
     if (@user && !!@user.authenticate(params["password"]))
       render json: {
