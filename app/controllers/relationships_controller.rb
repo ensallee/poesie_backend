a0d7b4753416717193268649ab721752f235827c
ensallee <ensallee@gmail.com>
2018-07-25 15:50:30 -0400

    set up custom route for users to unfollow one another

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,4 +7,13 @@ class RelationshipsController < ApplicationController
 
     render json: User.all
   end
+
+  def destroy_relationship
+
+    @current_user = User.find(params[:follower_id])
+    @followed_user = User.find(params[:followed_id])
+    @current_user.unfollow(@followed_user)
+
+    render json: User.all
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :poems
   resources :users, only: [:index, :show, :create]
   resources :sessions, only: [:create]
-  resources :relationships
+  resources :relationships, except: [:destory]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/users/:id/poems', to: 'users#poems'
+  delete '/relationships', to: 'relationships#destroy_relationship'
 end
