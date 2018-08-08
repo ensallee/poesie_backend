e011d1bd351e39f723a493d71cc2d18a52a8e7ab
ensallee <ensallee@gmail.com>
2018-08-08 17:49:52 -0400

    added readme and adjusted seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "ensallee", password: "password", display_name: "Betsy", hometown: "Brooklyn", bio: "poet and web developer")
-User.create(username: "nwsufrin", password: "password", display_name: "Nat", hometown: "Brooklyn", bio: "poet and therapist")
-User.create(username: "mminton", password: "password", display_name: "Mark", hometown: "New York", bio: "poet and teacher")
+User.create(username:"guest", password: "password", display_name: "Guest", hometown: "New York, NY", bio: "Guest User")
 User.create(username: "splath", password: "password", display_name: "Sylvia Plath", hometown: "Boston", bio: "American poet, novelist, and short-story writer")
 User.create(username: "aginsberg", password: "password", display_name: "Allen Ginsberg", hometown: "New York", bio: "American poet, philosopher, writer, and activist. Beat")
 User.create(username: "tberrigan", password: "password", display_name: "Ted Berrigan", hometown: "New York", bio: "American poet. Who would have thought that I'd be here?")
